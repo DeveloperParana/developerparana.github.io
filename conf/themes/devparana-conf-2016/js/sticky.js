c1c233b73682e42670f46ea95318b54761637b14
Fernando Fabricio dos Santos <ffsantos@uolinc.com>
2016-10-25 16:42:13 -0200

    Removendo site da conf e adicionando banner na home

--- conf/themes/devparana-conf-2016/js/sticky.js
+++
@@ -1,9 +0,0 @@
-function sticky() {
-  if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
-      document.getElementById("nav").className = "fixed";
-  } else {
-      document.getElementById("nav").className = "";
-  }
-}
-
-window.onscroll = function() { sticky() };
